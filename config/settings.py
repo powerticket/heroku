7938f475056397909a80632895b92d6efa74a9fc
powerticket <jwp0530@gmail.com>
2020-10-20 00:17:48 +0900

    add ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'MY_SECREY_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['powerticket.herokuapp.com']
 
 
 # Application definition
