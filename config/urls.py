411f06e84d9a43809d379f5d2ac0db3ba8517b8f
powerticket <jwp0530@gmail.com>
2020-10-21 00:41:06 +0900

    add static url

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from index import views as index_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_views.index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
