945ca3952d5cecd5dceecc8b2e563f5602d5c61d
powerticket <jwp0530@gmail.com>
2020-10-21 00:37:32 +0900

    edit STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static_root/'
+STATIC_ROOT = BASE_DIR / 'static_root'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
