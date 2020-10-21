6600507e7cf4b97401f33984ba032abc143ac43c
powerticket <jwp0530@gmail.com>
2020-10-21 21:52:58 +0900

    add accounts app

---
+++ accounts/views.py
@@ -0,0 +1,55 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import MyUserCreationForm
+
+
+# Create your views here.
+def signup(request):
+    if request.method == "POST":
+        signup_form = MyUserCreationForm(request.POST)
+        if signup_form.is_valid():
+            user = signup_form.save()
+            auth_login(request, user)            
+            return redirect('index')
+    else:
+        signup_form = MyUserCreationForm()
+    context = {
+        'signup_form': signup_form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+def signout(request):
+    pass
+
+
+def login(request):
+    if request.method == "POST":
+        login_form = AuthenticationForm(request, request.POST)
+        if login_form.is_valid():
+            auth_login(request, login_form.get_user())
+            return redirect('index')
+    else:
+        login_form = AuthenticationForm()
+    context = {
+        'login_form': login_form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('index')
+
+
+def profile(request, user_pk):
+    User = get_user_model()
+    user = get_object_or_404(User, pk=user_pk)
+    context = {
+        'user': user
+    }
+    return render(request, 'accounts/profile.html', context)
--- config/urls.py
+++ config/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from index import views as index_views
+from django.contrib import admin
+from django.urls import include, path
 
+from index import views as index_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_views.index, name='index'),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
